0d56de0c667a046ba82247481825d1bf639c9927
Rizky <rizky05@gmail.com>
2017-09-15 12:22:54 +0700

    adding tab context-menu

--- pages/Index/Tab/Tab.js
+++ pages/Index/Tab/Tab.js
@@ -7,3 +7,26 @@ this.closeTab = (item, e) => {
     e.preventDefault();
     this.props.updateTabs(item._close());
 }
+
+this.contextMenu = [{
+    label: 'Close',
+    icon: '',
+    props: (tabItem, menuItem, hide) => {
+        return {
+            onClick: (e) => {
+                this.props.updateTabs(tabItem._close());
+                hide();
+            }
+        }
+    }
+}];
+this.contextMenuProps = (treeItem, menuItem, hide) => {
+    if (typeof menuItem.props === 'function') {
+        return menuItem.props(treeItem, menuItem, hide);
+    }
+    return {
+        onClick: (e) => {
+            hide();
+        }
+    };
+};
